6ba789b21b59f30c1b70aba17592f6315bdf16d8
sjankosky <sjankosky@gmail.com>
2021-06-26 15:06:51 -0400

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ocean from "./ocean.jpg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">HELLO</header>
+      <img src={ocean} className="img-ocean" alt="ocean" />
+    </div>
+  );
+}
