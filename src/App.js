23f07de8462466e6143817a1f5cc9e58e50fee0d
sjankosky <sjankosky@gmail.com>
2021-06-26 21:49:25 -0400

    Adding basic search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import ocean from "./ocean.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">HELLO</header>
-      <img src={ocean} className="img-ocean" alt="ocean" />
+      <div className="container">
+        <header className="App-header">
+          HELLO
+          <img src={ocean} className="img-ocean" alt="ocean" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Sarah Jankosky</footer>
+      </div>
     </div>
   );
 }
